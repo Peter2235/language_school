efdd83f508327317f5a902d82884913675e8eb68
Peter Tirala <peter.tirala@inqool.cz>
2017-10-19 19:13:19 +0200

    Project start

---
+++ src/main/java/PA165/language_school_manager/LanguageSchoolManagerApplication.java
@@ -0,0 +1,12 @@
+package PA165.language_school_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageSchoolManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanguageSchoolManagerApplication.class, args);
+    }
+}
