9ded53c5a52519e7aac43d9003dd954bf16089dc
Matúš Sedlák <433342@mail.muni.cz>
2017-10-22 16:52:09 +0200

    vytvorene entity, ciastocna implementacia

---
+++ src/main/java/PA165/Entities/Lecture.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PA165.Entities;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+/**
+ *
+ * @author Matúš Sedlák
+ */
+@Entity
+public class Lecture {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date time;
+
+    @ManyToOne
+    @NotNull
+    private Course course;
+    
+    private String topic;
+    
+    @ManyToOne
+    @NotNull
+    private Lecturer lecturer;
+    
+    @ManyToMany(mappedBy = "lecture")
+    private Set<Person> students = new HashSet<Person>();  
+
+    public void addStudent(Person student) {
+		this.students.add(student);
+    }
+
+    public Set<Person> getStudents() {
+        return Collections.unmodifiableSet(students);
+    }
+    
+    public Lecture(){   
+    }
+    
+    public Lecture(long id){
+        this.id = id;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public Lecturer getLecturer() {
+        return lecturer;
+    }
+
+    public void setLecturer(Lecturer lecturer) {
+        this.lecturer = lecturer;
+    }
+    
+}
---
+++ src/main/java/PA165/Entities/Person.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PA165.Entities;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+/**
+ *
+ * @author Matúš Sedlák
+ */
+@Entity
+public class Person{
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+    
+    private String firstName;
+    private String middleName;
+    
+    @NotNull
+    @Column(nullable=false)
+    private String lastNamel;
+    
+}
