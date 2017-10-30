4340905dd083bb5f3b4805eceb53f1a2e3edf6a8
Matúš Sedlák <433342@mail.muni.cz>
2017-10-30 17:37:12 +0100

    javadoc for lecturedao interface

--- src/main/java/PA165/language_school_manager/Dao/LectureDao.java
+++ src/main/java/PA165/language_school_manager/Dao/LectureDao.java
@@ -14,10 +14,42 @@ import java.util.List;
  * @author Matúš Sedlák
  */
 public interface LectureDao {
+    
+    /**
+     * Get lecture with given id
+     * @param id - id of lecture
+     * @return lecture with given id
+     */
     public Lecture findById(Long id);
+    
+    /**
+     * Store lecture given as parameter in database
+     * @param l - lecture to be stored in database
+     */
     public void create(Lecture l);
+    
+    /**
+     * Delete lecture from database
+     * @param l - lecture which should be deleted
+     */
     public void delete(Lecture l);
+    
+    /**
+     * Update the lecture information in database
+     * @param l - Lecture which should be updated
+     */
     public void update(Lecture l);
+    
+    /**
+     * Retrieve all lectures from database
+     * @return List of lectures stored in database
+     */
     public List<Lecture> findAll();
+    
+    /**
+     * Get lecture with given topic (every lecture has a unique topic)
+     * @param l - Topic of the searched lecture
+     * @return Lecture with given topic
+     */
     public Lecture findByTopic(String l);
 }
