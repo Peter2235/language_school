f4487c23ce131f5aa5779a99b4ea0f46103fc19d
Mantook21 <slanyviktor@gmail.com>
2017-11-27 19:46:13 +0100

    person tests

--- dao/src/main/java/PA165/language_school_manager/Entities/Person.java
+++ dao/src/main/java/PA165/language_school_manager/Entities/Person.java
@@ -52,6 +52,10 @@ public class Person {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getFirstName() {
         return firstName;
     }
