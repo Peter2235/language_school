2d8096f1b80ef44804263223de78699428cfbc7d
Matúš Sedlák <sedlak87@gmail.com>
2017-11-27 16:47:03 +0100

    added lecturerservicetest added create delete update to personservice

--- service/src/main/java/PA165/language_school_manager/service/PersonServiceImpl.java
+++ service/src/main/java/PA165/language_school_manager/service/PersonServiceImpl.java
@@ -7,6 +7,7 @@ package PA165.language_school_manager.service;
 
 import PA165.language_school_manager.Dao.PersonDao;
 import PA165.language_school_manager.Entities.Person;
+import PA165.language_school_manager.LanguageSchoolException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -14,11 +15,11 @@ import org.springframework.beans.factory.annotation.Autowired;
  *
  * @author Matúš
  */
-public class PersonServiceImpl implements PersonService{
+public class PersonServiceImpl implements PersonService {
 
     @Autowired
     private PersonDao personDao;
-    
+
     @Override
     public List<Person> findAll() {
         return personDao.findAll();
@@ -33,9 +34,36 @@ public class PersonServiceImpl implements PersonService{
     public Person findPersonByUserName(String userName) {
         return personDao.findByUserName(userName);
     }
-    
+
     @Override
     public List<Person> findPersonsByLastName(String lastName) {
         return personDao.findByLastName(lastName);
-    }  
+    }
+
+    @Override
+    public void createPerson(Person person) {
+        try {
+            personDao.create(person);
+        } catch (Throwable e) {
+            throw new LanguageSchoolException("Person create failed");
+        }
+    }
+
+    @Override
+    public void updatePerson(Person person) {
+        try {
+            personDao.update(person);
+        } catch (Throwable e) {
+            throw new LanguageSchoolException("Person update failed");
+        }
+    }
+
+    @Override
+    public void deletePerson(Person person) {
+        try {
+            personDao.delete(person);
+        } catch (Throwable e) {
+            throw new LanguageSchoolException("Person delete failed");
+        }
+    }
 }
