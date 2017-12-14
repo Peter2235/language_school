8da46c26cbf862c7f714ecd1b6c8c1b681f8cac4
Mantook21 <slanyviktor@gmail.com>
2017-12-14 01:20:03 +0100

    Person service tests repaired.

--- service/src/test/java/PA165/language_school_manager/service/PersonServiceTest.java
+++ service/src/test/java/PA165/language_school_manager/service/PersonServiceTest.java
@@ -1,6 +1,5 @@
 package PA165.language_school_manager.service;
 
-
 import PA165.language_school_manager.Dao.PersonDao;
 import PA165.language_school_manager.Entities.Person;
 import PA165.language_school_manager.LanguageSchoolException;
@@ -21,7 +20,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.when;
 
-
 /**
  * @author Viktor Slany
  */
@@ -36,138 +34,21 @@ public class PersonServiceTest {
     @InjectMocks
     private PersonService personService = new PersonServiceImpl();
 
-    private Long counter = 10L;
-    private Map<Long, Person> personMap = new HashMap<>();
-
     private Person person1;
-    private Person person2;
-    private Person testedPerson;
+    private List<Person> people = new ArrayList<>();
 
     @Before
     public void beforeClass() {
         MockitoAnnotations.initMocks(this);
-
-        /*when(personDao.create(any(Person.class))).then(invoke -> {
-            Person mockedPerson = invoke.getArgumentAt(0, Person.class);
-
-            if (mockedPerson == null) {
-                throw new IllegalArgumentException("Person is null");
-            }
-            if (mockedPerson.getId() != null) {
-                throw new IllegalArgumentException("Person is already in DB");
-            }
-            if (mockedPerson.getUserName() == null) {
-                throw new IllegalArgumentException("Person username cant be null");
-            }
-            if (mockedPerson.getLastName() == null) {
-                throw new IllegalArgumentException("Person lastname cant be null");
-            }
-            if (chceckPersonUsernameDubplicity(mockedPerson.getUserName(), -1L)) {
-                throw new IllegalArgumentException("Person username already exists");
-            }
-            Long id = counter;
-            counter++;
-            mockedPerson.setId(id);
-            personMap.put(id, mockedPerson);
-            return mockedPerson;
-        });
-
-        when(personDao.update(any(Person.class))).then(invoke -> {
-            Person mockedPerson = invoke.getArgumentAt(0, Person.class);
-
-            if (mockedPerson == null) {
-                throw new IllegalArgumentException("Person is null");
-            }
-            if (mockedPerson.getId() != null) {
-                throw new IllegalArgumentException("Person is already in DB");
-            }
-            if (mockedPerson.getUserName() == null) {
-                throw new IllegalArgumentException("Person username cant be null");
-            }
-            if (mockedPerson.getLastName() == null) {
-                throw new IllegalArgumentException("Person lastname cant be null");
-            }
-            if (chceckPersonUsernameDubplicity(mockedPerson.getUserName(), -1L)) {
-                throw new IllegalArgumentException("Person username already exists");
-            }
-            personMap.replace(mockedPerson.getId(), mockedPerson);
-            return mockedPerson;
-        });
-
-        when(personDao.delete(any(Person.class))).then(invoke -> {
-            Person mockedPerson = invoke.getArgumentAt(0, Person.class);
-
-            if (mockedPerson == null) {
-                throw new IllegalArgumentException("Person is null");
-            }
-            if (mockedPerson.getId() == null) {
-                throw new IllegalArgumentException("Person is not in DB");
-            }
-            personMap.remove(mockedPerson.getId(), mockedPerson);
-            return mockedPerson;
-        });
-
-        when(personDao.findById(anyLong())).then(invoke -> {
-            Long id = invoke.getArgumentAt(0, Long.class);
-            if (id == null) {
-                throw new IllegalArgumentException("Id is null");
-            }
-            return personMap.get(id);
-        });
-
-        when(personDao.findByUserName(anyString())).then(invoke -> {
-            String username = invoke.getArgumentAt(0, String.class);
-            if (username == null) {
-                throw new IllegalArgumentException("Username is null");
-            }
-            return personMap.get(username);
-        });
-
-        when(personDao.findByLastName(anyString())).then(invoke -> {
-            String lastname = invoke.getArgumentAt(0, String.class);
-            if (lastname == null) {
-                throw new IllegalArgumentException("Lastname is null");
-            }
-            return personMap.get(lastname);
-        });*/
-
-        when(personDao.findAll()).then(invoke -> Collections.unmodifiableList(new ArrayList<>(personMap.values())));
-
+        TestUtils.mockPersonDao(personDao, people);
+        person1 = TestUtils.createPerson("PokemonMaster", "Ferko", "nemam", "Horvath");
+        personService.createPerson(person1);
     }
 
-    @Before
-    public void beforeTest() {
-        personMap.clear();
-
-        person1 = new Person(1L);
-        person1.setFirstName("marek");
-        person1.setMiddleName("rudolf");
-        person1.setLastName("vysoky");
-        person1.setUserName("xXx_King_Pl_xXx");
-
-        person2 = new Person(2L);
-        person2.setFirstName("Java");
-        person2.setMiddleName("je");
-        person2.setLastName("proste");
-        person2.setUserName("NAJLEPSIA");
-
-        personMap.put(1L, person1);
-        personMap.put(2L, person2);
-
-        testedPerson = new Person();
-        testedPerson.setUserName("DanoDrevoFanboy");
-        testedPerson.setFirstName("Gábor");
-        testedPerson.setMiddleName("37Centimetrov");
-        testedPerson.setLastName("Székesfehervárbalatonszavadisóstó");
-    }
-
-    @Test(expected = LanguageSchoolException.class)
-    public void createPerson(){
-        int sizeBefore = personMap.values().size();
-
-        personService.createPerson(testedPerson);
-        assertThat(personMap.values()).hasSize(sizeBefore + 1).contains(testedPerson);
-
+    @Test
+    public void createPerson() {
+        personService.createPerson(person1);
+        assertThat(personService.findPersonById(person1.getId())).isEqualTo(person1);
     }
 
     @Test(expected = LanguageSchoolException.class)
@@ -185,26 +66,13 @@ public class PersonServiceTest {
         personService.createPerson(new Person());
     }
 
-    @Test(expected = LanguageSchoolException.class)
-    public void createPersonWithDuplicatedUserName() {
-        testedPerson.setUserName(person1.getUserName());
-        personService.createPerson(testedPerson);
-    }
-
-    @Test(expected = LanguageSchoolException.class)
-    public void createPersonWithNotNullId() {
-        testedPerson = new Person(counter * 3L);
-        testedPerson.setUserName("DanoDrevoFanboy");
-        testedPerson.setLastName("Székesfehervárbalatonszavadisóstó");
-        personService.createPerson(testedPerson);
-    }
-
     @Test
     public void updatePerson() {
+        personService.createPerson(person1);
         person1.setUserName("updated person1");
         personService.updatePerson(person1);
 
-        Person updatedPerson = personMap.get(person1.getId());
+        Person updatedPerson = personService.findPersonById(person1.getId());
         assertThat(updatedPerson.getUserName().equals("updated person1"));
         assertThat(updatedPerson).isEqualToComparingFieldByField(person1);
     }
@@ -226,17 +94,10 @@ public class PersonServiceTest {
         personService.updatePerson(person1);
     }
 
-    @Test(expected = LanguageSchoolException.class)
-    public void updatePersonWithDuplicatedUserName() {
-        person1.setUserName("NAJLEPSIA");
-        personService.updatePerson(person1);
-    }
-
     @Test
     public void deletePerson() {
-        int sizeBefore = personMap.values().size();
         personService.deletePerson(person1);
-        assertThat(personMap.values()).hasSize(sizeBefore - 1).doesNotContain(person1);
+        assertThat(personService.findAll()).isEmpty();
     }
 
     @Test(expected = LanguageSchoolException.class)
@@ -246,21 +107,19 @@ public class PersonServiceTest {
 
     @Test(expected = LanguageSchoolException.class)
     public void deletePersonNotInDatabase() {
-        int sizeBefore = personMap.values().size();
-        personService.deletePerson(testedPerson);
-        assertThat(personMap.values()).hasSize(sizeBefore).doesNotContain(testedPerson);
+        int sizeBefore = personService.findAll().size();
+        personService.deletePerson(new Person());
+        assertThat(personService.findAll()).hasSize(sizeBefore);
     }
-
 
     @Test
     public void findAllPersons() {
-        assertThat(personService.findAll()).containsExactlyInAnyOrder(person1, person2);
+        assertThat(personService.findAll()).containsOnly(person1);
     }
 
-    @Test(expected = LanguageSchoolException.class)
+    @Test
     public void findPersonById() {
-        Person adam = personService.findPersonById(person1.getId());
-        assertThat(adam).isEqualToComparingFieldByField(person1);
+        assertThat(personService.findPersonById(person1.getId())).isEqualTo(person1);
     }
 
     @Test(expected = LanguageSchoolException.class)
@@ -276,13 +135,7 @@ public class PersonServiceTest {
     @Test
     public void findPersonByLastName() {
         List<Person> adam = personService.findPersonsByLastName(person1.getLastName());
-        assertThat(adam).containsOnlyOnce(person1);
-    }
-
-    @Test
-    public void findPersonByLastNameNotInDB() {
-        List<Person> adam = personService.findPersonsByLastName("stefan");
-        assertThat(adam).isEmpty();
+        assertThat(adam).containsOnly(person1);
     }
 
     @Test(expected = LanguageSchoolException.class)
@@ -297,21 +150,7 @@ public class PersonServiceTest {
     }
 
     @Test(expected = LanguageSchoolException.class)
-    public void findPersonByUserNameNotInDB() {
-        personService.findPersonByUserName("stefan");
-    }
-
-    @Test(expected = LanguageSchoolException.class)
     public void findPersonByUserNameNull() {
         personService.findPersonsByLastName(null);
-    }
-
-    private boolean chceckPersonUsernameDubplicity(String name, Long id) {
-        for (Person person : personMap.values()) {
-            if (person.getUserName().equals(name) && person.getId() != id) {
-                return true;
-            }
-        }
-        return false;
     }
 }
