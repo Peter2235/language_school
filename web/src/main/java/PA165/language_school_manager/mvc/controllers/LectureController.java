e4f37540c41327c699d3d0a539eaf7b0ff3ed6f0
Matúš Sedlák <sedlak87@gmail.com>
2017-12-16 11:30:42 +0100

    controller updated

--- web/src/main/java/PA165/language_school_manager/mvc/controllers/LectureController.java
+++ web/src/main/java/PA165/language_school_manager/mvc/controllers/LectureController.java
@@ -11,7 +11,6 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -22,7 +21,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.util.UriComponentsBuilder;
-import java.lang.StringBuilder;
 
 /**
  *
