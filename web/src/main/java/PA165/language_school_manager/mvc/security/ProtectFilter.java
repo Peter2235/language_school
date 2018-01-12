6c37ac9dfd21a3d770fd79010a8ed660c04fc175
Peter Tirala <peter.tirala@inqool.cz>
2018-01-12 11:02:50 +0100

    protect filter

--- web/src/main/java/PA165/language_school_manager/mvc/security/ProtectFilter.java
+++ web/src/main/java/PA165/language_school_manager/mvc/security/ProtectFilter.java
@@ -5,30 +5,23 @@
  */
 package PA165.language_school_manager.mvc.security;
 
-import PA165.language_school_manager.DTO.PersonAuthenticateDTO;
 import PA165.language_school_manager.DTO.PersonDTO;
-import PA165.language_school_manager.Entities.Person;
-import PA165.language_school_manager.Facade.PersonFacade;
-import java.io.IOException;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.bind.DatatypeConverter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.context.support.WebApplicationContextUtils;
+import java.io.IOException;
 
 /**
  *
  * @author Matúš
  */
-@WebFilter(urlPatterns = {"/lecture/new", "/lecture/delete/*", "/course/new", "/course/create", "/course/delete/*"})
+@WebFilter(urlPatterns = {"/lecture/new", "/lecture/create", "/lecture/edit/*", "/lecture/delete/*", "/course/new", "/course/create", "/course/delete/*",
+        "/lecturer/new", "/lecturer/create", "/lecturer/edit/*", "/lecturer/delete/*"
+})
 public class ProtectFilter implements Filter {
 
     private final static Logger log = LoggerFactory.getLogger(ProtectFilter.class);
